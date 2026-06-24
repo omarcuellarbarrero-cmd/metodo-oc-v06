ed4e5f11f58055f18eac3f9fbdcb26ffb6cd0fba
Omar Cuellar Barrero <omarcuellarbarrero@gmail.com>
2026-06-24 06:06:27 -0500

    Configuracion para Coolify

---
+++ server.js
@@ -0,0 +1,11 @@
+import express from "express";
+
+const app = express();
+
+app.use(express.static("public"));
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Servidor iniciado en puerto ${PORT}`);
+});
